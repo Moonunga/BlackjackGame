45916c1ba7b81fa49fe85e91d298b1bfc8e9dfdf
Moonunga <101837977+Moonunga@users.noreply.github.com>
2022-11-03 00:08:30 -0400

    Git Setting

---
+++ BlackJackLibrary/Card.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlackJackLibrary
+{
+    public enum CardSuit
+    { Spades, Hearts, Clubs, Diamonds }
+    public enum CardFace
+    { A, two, three, four, five, six, seven, eight, nine, ten, J, Q, K }
+
+    internal class Card
+    {
+        public CardSuit Suit { get; private set; }
+        public CardFace Face { get; private set; }
+
+
+        public Card(CardSuit suit,CardFace face)
+        {
+            Suit = suit;
+            Face = face;
+        }
+
+
+    }
+}
---
+++ BlackJackLibrary/Deck.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlackJackLibrary
+{
+   
+    internal class Deck
+    {
+        protected List<Card> cards = new List<Card>();
+
+        public Deck()
+        {
+            CreateDeck();
+        }
+
+        public void CreateDeck()
+        {
+            foreach (CardSuit cardSuit in Enum.GetValues(typeof(CardSuit)))
+            {
+                foreach (CardFace cardface in Enum.GetValues(typeof(CardFace)))
+                {
+                    
+                }
+            }
+
+        }
+
+
+    }
+}
